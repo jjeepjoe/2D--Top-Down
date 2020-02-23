6525ca361818dc3ef250b96b5a56de20b02cb5cc
Joseph Mazola <jjeepjoe@hotmail.com>
2020-02-23 03:57:09 -0600

    Initial push

---
+++ Assets/SCripts/MovementController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    //CONFIG PARAMS
+    const string ANIMATIONSTATE = "AnimationState";
+    public float movementSpeed = 3.0f;
+    Vector2 movement = new Vector2();
+    Animator myAnimator;
+    Rigidbody2D myRb2D;
+    //OUR OWN DATA TYPE
+    enum CharStates
+    {
+        walkEast = 1,
+        walkSouth = 2,
+        walkWest = 3,
+        walkNorth = 4,
+        idleSouth = 5
+    }
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+        myRb2D = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        UpdateState();
+    }
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+    private void MoveCharacter()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        movement.Normalize();
+
+        myRb2D.velocity = movement * movementSpeed;
+    }
+    private void UpdateState()
+    {
+        if(movement.x > 0)
+        {
+            myAnimator.SetInteger(ANIMATIONSTATE, (int)CharStates.walkEast);
+        }
+        else if(movement.x < 0)
+        {
+            myAnimator.SetInteger(ANIMATIONSTATE, (int)CharStates.walkWest);
+        }
+        else if(movement.y < 0)
+        {
+            myAnimator.SetInteger(ANIMATIONSTATE, (int)CharStates.walkSouth);
+        }
+        else if(movement.y > 0)
+        {
+            myAnimator.SetInteger(ANIMATIONSTATE, (int)CharStates.walkNorth);
+        }
+        else
+        {
+            myAnimator.SetInteger(ANIMATIONSTATE, (int)CharStates.idleSouth);
+        }
+    }
+
+}
---
+++ Assets/SCripts/RoundCameraPos.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Cinemachine;
+
+public class RoundCameraPos : CinemachineExtension
+{
+    //CONFIG PARAMS
+    public float PixelPerUnit = 32;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, 
+                    CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+        if(stage == CinemachineCore.Stage.Body)
+        {
+            Vector3 pos = state.FinalPosition;
+
+            Vector3 pos2 = new Vector3(Round(pos.x), Round(pos.y), pos.z);
+
+            state.PositionCorrection += pos2 - pos;
+        }
+    }
+    private float Round(float x)
+    {
+        return Mathf.Round(x * PixelPerUnit) / PixelPerUnit;
+    }
+}
