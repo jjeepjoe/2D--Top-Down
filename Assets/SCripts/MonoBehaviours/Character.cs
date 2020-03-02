d76bda897771871d1686881b422097ecd3e15bec
Joseph Mazola <jjeepjoe@hotmail.com>
2020-03-02 15:16:03 -0600

    Should be updated

---
+++ Assets/SCripts/MonoBehaviours/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //CONFIG PARAMS
+    public int hitPoints;
+    public int maxHitPoints;
+}
---
+++ Assets/SCripts/MonoBehaviours/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    //CONFIG PARAMS
+
+
+    void OnTriggerEnter2D(Collider2D otherColllider)
+    {
+        if (otherColllider.gameObject.CompareTag("CanBePickedUp"))
+        {
+            otherColllider.gameObject.SetActive(false);
+        }
+    }
+}
+
