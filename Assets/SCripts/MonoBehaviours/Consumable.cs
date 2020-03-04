b86ed8bd6fad4c6c9a1ab8f53f23c3909f77fef1
jjeepjoe <jjeepjoe@hotmail.com>
2020-03-03 19:47:53 -0600

    Up to the scriptable objects and now the health bar canvas.

---
+++ Assets/SCripts/MonoBehaviours/Consumable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : MonoBehaviour
+{
+    //CONFIG PARAMS
+    public Item consumableItem;  //A HANDLE TO OUR SCRIPTABLE OBJECT.
+
+}
--- Assets/SCripts/MonoBehaviours/Player.cs
+++ Assets/SCripts/MonoBehaviours/Player.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class Player : Character
 {
     //CONFIG PARAMS
-
+    //WE INHERIT THE CLASS VARIABLES HITPOINTS FROM THE CHARACTER CLASS
 
     void OnTriggerEnter2D(Collider2D otherColllider)
     {
         if (otherColllider.gameObject.CompareTag("CanBePickedUp"))
         {
-            otherColllider.gameObject.SetActive(false);
+            Item hitObject = otherColllider.gameObject.GetComponent<Consumable>().consumableItem;
+            if(hitObject != null)
+            {
+                print("it: " + hitObject.objectName);
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        break;
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+                otherColllider.gameObject.SetActive(false);
+            }
         }
+    }
+    //
+    public void AdjustHitPoints(int amount)
+    {
+        hitPoints = hitPoints + amount;
+        print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
     }
 }
 
